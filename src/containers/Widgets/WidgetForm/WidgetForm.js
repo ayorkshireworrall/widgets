5c47abac0fea532db44fe4c93d98ffa7c76d1c60
aworrall <alex.worrall@keytree.co.uk>
2020-08-14 17:50:29 +0100

    A bit more styling for the widget page

--- src/containers/Widgets/WidgetForm/WidgetForm.js
+++ src/containers/Widgets/WidgetForm/WidgetForm.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
+import classes from './WidgetForm.module.css';
+
 const WidgetForm = props => {
     return (
-        <form>
-            <input/>
-            <input/>
-            <input type="submit"/>
-        </form>
+        <div className={classes.WidgetForm}>
+            <h1>Add a Widget</h1>
+            <form>
+                <span>Name:</span>
+                <input/>
+                <span>Description:</span>
+                <textarea/>
+                <input type="submit" value="Add"/>
+            </form>
+        </div>
     )
 }
 
