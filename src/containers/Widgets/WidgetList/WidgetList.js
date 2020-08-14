305a9db122b744ee84526566dcee25fa8b867c19
aworrall <alex.worrall@keytree.co.uk>
2020-08-14 17:12:39 +0100

    Fix a few styling issues with list

--- src/containers/Widgets/WidgetList/WidgetList.js
+++ src/containers/Widgets/WidgetList/WidgetList.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import ListItem from './ListItem/ListItem';
 
+import classes from './WidgetList.module.css';
+
 const WidgetList = props => {
 
     return (
-        <ul>
+        <ul className={classes.WidgetList}>
             {props.widgets.map(widget => <ListItem name={widget.name} description={widget.description}/>)}
         </ul>
     )
